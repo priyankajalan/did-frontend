526f56748bc06502e402b466dd5f6efb46a502e1
Priyanka Jalan <priyanka.jalan@axa.com.sg>
2018-10-15 00:24:22 +0800

    ui added

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import BotImage from './bot.png';
 // import Dictaphone from './Dictaphone';
 import Script from './Script';
 
 const MainApp = document.getElementById('app');
 
 ReactDOM.render(
-        <div>
+        <div style={{textAlign:'center',padding:'50px'}}>
+            <h1 style={{color:'#26a8e0'}}>Amica</h1>
+            <img src={BotImage} />
+            <h3 style={{color:'#26a8e0'}}>
+            Speak to me anytime when you feel lonely and I will be your friend always</h3>
+            <h5 style={{color:'#26a8e0'}}>Presented by<br />
+            <span style={{fontSize:'18px'}}> ISS-NUS</span><br />
+            Digital Innovations and Design</h5>
             <Script />
         </div>,
     MainApp
